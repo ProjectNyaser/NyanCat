8357c8390b4b380e77868328426345650513642d
SomeoneProject <3277673229@qq.com>
2020-04-18 15:16:51 +0800

    添加项目文件。

---
+++ NyanCat.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace NyanCat
+{
+    partial class NyanCat
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NyanCat));
+            this.label1 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(160)))));
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(720, 400);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\r\nSTOP: c000002 Unknown Hard Error\r\nUnknow Hard Error";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(0, 0);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // NyanCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(720, 400);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.TopMost = true;
+            this.Name = "NyanCat";
+            this.ShowInTaskbar = false;
+            this.Text = "NyanCat";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ NyanCat.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace NyanCat
+{
+    public partial class NyanCat : Form
+    {
+        public NyanCat()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hide();
+            Point MousePoint = MousePosition;
+            int Frame = 0;
+            string text = "Your computer has been trashed by the MEMZ Trojan. Now enjoy the Nyan Cat...";
+            File.WriteAllBytes($"{Environment.GetEnvironmentVariable("Temp")}\\Minecraft.ttf", Properties.Resources.Minecraft);
+            File.WriteAllBytes($"{Environment.GetEnvironmentVariable("Temp")}\\Nyan_Cat_Prelude.mp3", Properties.Resources.Nyan_Cat_Prelude);
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.Nyan_Cat);
+            MouseClick += Exit;
+            label1.MouseClick += Exit;
+            MouseWheel += Exit;
+            label1.MouseWheel += Exit;
+            MouseMove += MouseMoved;
+            label1.MouseMove += MouseMoved;
+            LostFocus += LoseFocus;
+            label1.LostFocus += LoseFocus;
+            axWindowsMediaPlayer1.LostFocus += LoseFocus;
+            KeyDown += Exit;
+            void Exit(object a, EventArgs b) { Application.Exit(); }
+            void LoseFocus(object a, EventArgs b) { if (!Focused && !label1.Focused && !axWindowsMediaPlayer1.Focused) { Exit(a, b); } }
+            void MouseMoved(object a, EventArgs b) { if (MousePoint != MousePosition) { Exit(a, b); } }
+            Cursor.Hide();
+            label1.Location = new Point((Size.Width / 2) - 360, (Size.Height / 2) - 200);
+            System.Windows.Forms.Timer BG_Show = new System.Windows.Forms.Timer { Interval = 1 };
+            BG_Show.Tick += new EventHandler((object sender3, EventArgs e3) =>
+            {
+                BG_Show.Interval = 70;
+                if (Frame == 0)
+                {
+                    BackgroundImage = Properties.Resources._00;
+                }
+
+                if (Frame == 1)
+                {
+                    BackgroundImage = Properties.Resources._01;
+                }
+
+                if (Frame == 2)
+                {
+                    BackgroundImage = Properties.Resources._02;
+                }
+
+                if (Frame == 3)
+                {
+                    BackgroundImage = Properties.Resources._03;
+                }
+
+                if (Frame == 4)
+                {
+                    BackgroundImage = Properties.Resources._04;
+                }
+
+                if (Frame == 5)
+                {
+                    BackgroundImage = Properties.Resources._05;
+                }
+
+                if (Frame == 6)
+                {
+                    BackgroundImage = Properties.Resources._06;
+                }
+
+                if (Frame == 7)
+                {
+                    BackgroundImage = Properties.Resources._07;
+                }
+
+                if (Frame == 8)
+                {
+                    BackgroundImage = Properties.Resources._08;
+                }
+
+                if (Frame == 9)
+                {
+                    BackgroundImage = Properties.Resources._09;
+                }
+
+                if (Frame == 10)
+                {
+                    BackgroundImage = Properties.Resources._10;
+                }
+
+                if (Frame == 11)
+                {
+                    BackgroundImage = Properties.Resources._11;
+                }
+                if (Frame >= 11)
+                {
+                    Frame = 0;
+                }
+                else
+                {
+                    Frame += 1;
+                }
+            });//彩虹猫
+            System.Windows.Forms.Timer T_Show = new System.Windows.Forms.Timer { Interval = 20 };
+            T_Show.Tick += new EventHandler((object sender2, EventArgs e2) =>
+            {
+                try
+                {
+                    label1.Text = $"{text.Substring(0, label1.Text.Length)}_";
+                }
+                catch
+                {
+                    T_Show.Enabled = false;
+                    System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer
+                    {
+                        Interval = 1
+                    };
+                    for (int i = 0; i < 4; i++)
+                    {
+                        label1.Text = text;
+                        if (i % 2 == 0)
+                        {
+                            label1.Text += " ";
+                        }
+                        else
+                        {
+                            label1.Text += "_";
+                        }
+                        label1.Refresh();
+                        Thread.Sleep(250);
+                    };
+                    sp.PlayLooping();
+                    timer.Enabled = false;
+                    label1.Visible = false;
+                    BG_Show.Enabled = true;
+                }
+            });//打字机
+            System.Windows.Forms.Timer E_Show = new System.Windows.Forms.Timer { Interval = 2 };
+            E_Show.Tick += new EventHandler((object sender1, EventArgs e1) =>
+            {
+                if (E_Show.Interval == 1)
+                {
+                    if (axWindowsMediaPlayer1.status.Substring(0, 4) == "正在播放")
+                    {
+                        E_Show.Interval = 5000;
+                    }
+                    return;
+                }
+                if (E_Show.Interval == 2)
+                {
+                    axWindowsMediaPlayer1.URL = $"{Environment.GetEnvironmentVariable("Temp")}\\Nyan_Cat_Prelude.mp3";
+                    E_Show.Interval = 1;
+                    return;
+                }
+                E_Show.Enabled = false;
+                label1.AutoSize = true;
+                PrivateFontCollection p = new PrivateFontCollection();
+                p.AddFontFile($"{Environment.GetEnvironmentVariable("Temp")}\\Minecraft.ttf");
+                label1.Font = new Font(p.Families[0], (float)10.5, FontStyle.Regular);
+                label1.ForeColor = Color.Black;
+                label1.BackColor = Color.White;
+                label1.Text = "";
+                T_Show.Enabled = true;
+            });//蓝屏
+            E_Show.Enabled = true;
+            Show();
+        }
+    }
+}
